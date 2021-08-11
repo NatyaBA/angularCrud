238c509d0f1a39a52059e66c922e2a608a546050
NatyaBA <73196083+NatyaBA@users.noreply.github.com>
2021-08-11 21:36:15 +0300

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,17 +10,10 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'crud'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('crud');
   });
 
   it('should render title', () => {
